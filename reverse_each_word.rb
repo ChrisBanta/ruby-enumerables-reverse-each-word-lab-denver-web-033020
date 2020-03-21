a32234501eb6dfc9815a9f3be786072dd17e6faa
Chris Banta <chrisbanta1@gmail.com>
2020-03-21 18:05:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(string)
   new = string.split(" ")
-  reversed = new.each {|x| x.reverse!}
+  reversed = new.each {|word| word.reverse!}
 return reversed.join(" ")
 end
 
 def reverse_each_word(string)
   new = string.split(" ")
-  new.collect = {|x| x.reverse!}
+  new.collect {|word| word.reverse!}
   new.join(" ")
 end
